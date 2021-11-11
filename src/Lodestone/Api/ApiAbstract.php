5a03f10a62dabfd8f70d5a11d1b167357dbf8b0f
Flavien Normand <contact@flavien-normand.fr>
2021-11-11 22:45:23 +0100

    chore: default user agent for all requests

--- src/Lodestone/Api/ApiAbstract.php
+++ src/Lodestone/Api/ApiAbstract.php
@@ -26,6 +26,9 @@ class ApiAbstract
      */
     protected function handle(string $parser, array $requestOptions)
     {
+        if(!isset($requestOptions['user-agent'])) {
+            $requestOptions['user-agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36";
+        }
         $request = new Request($requestOptions);
 
         /** @var ResponseInterface $response */
